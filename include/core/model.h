0ae5c1b12d4d09767fc7dc3d7bb6077ea038836c
emilybready27 <emilybready27@gmail.com>
2022-03-26 17:43:00 -0500

    Added SortImagesByClassNumberCount method

--- include/core/model.h
+++ include/core/model.h
@@ -12,6 +12,10 @@ class Model {
   
  private:
   FileReader file_reader_;
+  std::vector<std::vector<Image>> images_;
+  std::vector<int> class_number_counts;
+  
+  void SortImagesByClassNumberCount(const std::vector<Image>& images);
   
 };
 
--- src/core/model.cc
+++ src/core/model.cc
@@ -8,10 +8,26 @@ Model::Model() {
 std::istream& operator>> (std::istream& input, Model& model) {
   model.file_reader_ = FileReader(input);
   std::vector<Image> images = model.file_reader_.GetImages();
+
+  model.SortImagesByClassNumberCount(images);
+  
+  
   
   return input;
 }
 
-
+void Model::SortImagesByClassNumberCount(const std::vector<Image>& images) {
+  // vector storing one vector of Images for each class number
+  images_ = std::vector<std::vector<Image>>(10); // TODO: magic number
+  // vector storing number of images in each class number
+  class_number_counts = std::vector<int>(10); // TODO: magic number
+  
+  for (const Image& image : images) {
+    int class_number = image.GetClassNumber();
+    class_number_counts[class_number]++;
+    images_[class_number].push_back(image);
+  }
+  
+}
 
 } // namespace naivebayes
