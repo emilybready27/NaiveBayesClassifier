e9af95a7880ca320054e44b861b6332e2ce3d333
emilybready27 <emilybready27@gmail.com>
2022-03-27 10:41:01 -0500

    Added FileWriter class

---
+++ include/core/file_writer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <fstream>
+#include "file_writer.h"
+
+namespace naivebayes {
+
+class FileWriter {
+ public:
+  FileWriter() = default;
+  
+//  FileWriter(std::ostream& output, std::string str);
+};
+
+//template<class T>
+//std::string ToString(const std::vector<T>& vector);
+
+} // namespace naivebayes
---
+++ src/core/file_writer.cc
@@ -0,0 +1,21 @@
+#include "core/file_writer.h"
+#include <iostream>
+
+namespace naivebayes {
+
+//FileWriter::FileWriter(std::ostream& output,
+//                       const std::vector<NumberClass>& number_classes,
+//                       ) {
+//  output << "hello";
+//}
+
+//template<class T>
+//std::string ToString(std::vector<T> vector) {
+//  std::string str;
+//  for (int i = 0; i < vector.size(); i++) {
+//    str += std::to_string(vector[i]) + " ";
+//  }
+//  return str;
+//}
+
+} // namespace naivebayes
